68662c0800ba9a929627c31eb92689e398470113
malikaatha <malika.atha@ui.ac.id>
2024-09-08 17:40:14 +0700

    selesai tugas 2

---
+++ main/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    @property
+    def is_price_expensive(self):
+        return self.price > 100000
---
+++ main/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase, Client
+from django.utils import timezone
+from .models import Product
+
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_nonexistent_page(self):
+        response = Client().get('/skibidi/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_strong_mood_user(self):
+        now = timezone.now()
+        prod = Product.objects.create(
+            name = "LABUBU",
+            price = 600000,
+            description = "gantungan kunci lucu nan mengerikan",
+        )
+        self.assertTrue(prod.is_price_expensive)
