7110292ec656ff2a30a337b3f94cae4b029a5786
malikaatha <malika.atha@ui.ac.id>
2024-09-17 23:12:22 +0700

    mau push

--- quirknook_pbp/settings.py
+++ quirknook_pbp/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://malika.atha31/quirknook", "malika.atha31/quirknook"]
