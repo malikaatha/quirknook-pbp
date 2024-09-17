d9c4ded9b0de08a36eda39ef27a15f95821814a4
malikaatha <malika.atha@ui.ac.id>
2024-09-17 23:30:18 +0700

    benerin url

--- quirknook_pbp/settings.py
+++ quirknook_pbp/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://malika.atha31/quirknook", "malika.atha31/quirknook"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://malika.atha31/quirknook", "https://malika.atha31/quirknook"]
