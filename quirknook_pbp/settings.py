2820c074383ca02c98ed982ee78ff15e3d85c2b6
malikaatha <malika.atha@ui.ac.id>
2024-09-24 21:13:48 +0700

    harusnya udah sih ini

--- quirknook_pbp/settings.py
+++ quirknook_pbp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h#3)l9ja9r7w8qclh9@28j5g$dd7(nixg!u9ig$c0_58%o^i=u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "malika-atha31-quirknook.pbp.cs.ui.ac.id"]
 
